3725b4b2b8662a9f64325e69c1f800438bcfea65
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-10-30 20:02:32 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_ENDPOINT: 'postgres://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q'
+    API_ENDPOINT: 'https://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q'
   }
