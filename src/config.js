0ef68260a0707cea87361a6d895c4134f6ed2df5
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-10-30 21:57:57 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    API_ENDPOINT: 'http://localhost:3000'
+    API_ENDPOINT: 'postgres://brfebwbluaohdy:08d1425cecd1afafdf782fa501879757bd5e04ad5c2a92386262074e518b9d6a@ec2-50-19-95-77.compute-1.amazonaws.com:5432/debnapqhhptg4q'
   }
